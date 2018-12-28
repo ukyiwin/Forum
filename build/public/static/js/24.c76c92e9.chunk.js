fa35dc8ce6dff1e9b7c0a90945f9fb992e2a9760
Nyein Chan Aung <forebus.pcone@gmail.com>
2018-12-28 09:43:33 +0630

    init

---
+++ build/public/static/js/24.c76c92e9.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{"86/s":function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Home")}}}]);
+//# sourceMappingURL=24.c76c92e9.chunk.js.map
